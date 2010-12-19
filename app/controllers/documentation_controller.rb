cb92443e8651c5540e42614d7e2a57f8301644c5
Tristan Dunn <tristanzdunn@gmail.com>
2010-12-19 15:03:29 -0500

    Use controller check rather than flash for warning to avoid setting a cookie.

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -1,7 +1,5 @@
 class DocumentationController < ApplicationController
   def show
-    flash.now[:warning] = 'The API is currently a work-in-progress and is subject to change without notice.'
-
     expires_in(1.week, :public => true)
 
     render :action => params[:id]
